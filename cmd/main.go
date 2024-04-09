cf411dd078f05aef9f23242b04ac6c5c79d3a62a
Santiago Saavedra <ssaavedraa@santiagosaavedra.com.co>
2024-04-09 01:55:00 -0400

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"santiagosaavedra.com.co/invoices/internal/api"
+)
+
+func main() {
+	if error := godotenv.Load(); error != nil {
+		log.Fatalf("Error loading .env file: %v", error)
+	}
+
+	router := gin.Default()
+
+	api.SetupRoutes(router)
+
+	port := os.Getenv("PORT")
+	address := ":" + port
+
+	if error := http.ListenAndServe(address, router); error != nil {
+		log.Fatalf("Failed to start server: %v", error)
+	}
+}
