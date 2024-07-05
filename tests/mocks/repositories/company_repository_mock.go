9f66ea3dc1642351c72d7046b70c6af156b3ba38
Santiago Saavedra <ssaavedraa@santiagosaavedra.com.co>
2024-07-06 05:14:56 +1000

    feat(mocks): mocks updated

--- tests/mocks/repositories/company_repository_mock.go
+++ tests/mocks/repositories/company_repository_mock.go
@@ -77,6 +77,62 @@ func (_c *MockCompanyRepository_FindByNameOrCreate_Call) RunAndReturn(run func(s
 	return _c
 }
 
+// Update provides a mock function with given fields: company
+func (_m *MockCompanyRepository) Update(company models.Company) (models.Company, error) {
+	ret := _m.Called(company)
+
+	if len(ret) == 0 {
+		panic("no return value specified for Update")
+	}
+
+	var r0 models.Company
+	var r1 error
+	if rf, ok := ret.Get(0).(func(models.Company) (models.Company, error)); ok {
+		return rf(company)
+	}
+	if rf, ok := ret.Get(0).(func(models.Company) models.Company); ok {
+		r0 = rf(company)
+	} else {
+		r0 = ret.Get(0).(models.Company)
+	}
+
+	if rf, ok := ret.Get(1).(func(models.Company) error); ok {
+		r1 = rf(company)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// MockCompanyRepository_Update_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'Update'
+type MockCompanyRepository_Update_Call struct {
+	*mock.Call
+}
+
+// Update is a helper method to define mock.On call
+//   - company models.Company
+func (_e *MockCompanyRepository_Expecter) Update(company interface{}) *MockCompanyRepository_Update_Call {
+	return &MockCompanyRepository_Update_Call{Call: _e.mock.On("Update", company)}
+}
+
+func (_c *MockCompanyRepository_Update_Call) Run(run func(company models.Company)) *MockCompanyRepository_Update_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(models.Company))
+	})
+	return _c
+}
+
+func (_c *MockCompanyRepository_Update_Call) Return(_a0 models.Company, _a1 error) *MockCompanyRepository_Update_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *MockCompanyRepository_Update_Call) RunAndReturn(run func(models.Company) (models.Company, error)) *MockCompanyRepository_Update_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // NewMockCompanyRepository creates a new instance of MockCompanyRepository. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewMockCompanyRepository(t interface {
